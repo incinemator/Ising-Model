7af8f4a6d3f27e16b3ad4c74e30595c6c3694861
incinemator <giannakzorz@gmail.com>
2024-04-04 12:21:37 +0200

    Calculate energy

--- ising.c
+++ ising.c
@@ -25,5 +25,11 @@ void initialize()
 // Calculate the energy of the lattice
 double calculateEnergy() 
 {
-
+    double energy = 0.0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            energy += -J * lattice[i][j] * (lattice[(i + 1) % N][j] + lattice[i][(j + 1) % N]);
+        }
+    }
+    return energy;
 }
