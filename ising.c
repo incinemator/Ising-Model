01b532c74442c15cce654e0a96a1682a57429d79
incinemator <giannakzorz@gmail.com>
2024-04-04 12:19:44 +0200

    Update

--- ising.c
+++ ising.c
@@ -8,3 +8,15 @@
 #define J 1.0 // Coupling constant
 #define bK 1.0 // Boltzmann consant
 #define T 2.0 // Temperature
+
+int lattice[N][N]
+
+// Initialize the lattice with random spins
+void initialize() {
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            lattice[i][j] = (rand() % 2) * 2 - 1; // Randomly assigns spin up or down
+        }
+    }
+}
